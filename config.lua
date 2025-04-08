410c79a5ee1eff0f68c2e7e606644b5c7dd2b38f
venozeiei <armcraftwar@gmail.com>
2025-04-08 10:13:06 +0700

    config.lua

---
+++ config.lua
@@ -0,0 +1,31 @@
+_G.Config = {
+    Delay = 1,  -- เวลาหน่วงระหว่างการทำงาน
+
+    -- ผลไม้ที่ต้องการตรวจสอบ
+    WantedFruits = {"Yeti-Yeti", "Leopard-Leopard", "Gas-Gas", "Kitsune-Kitsune"},
+    
+    -- สไตล์การต่อสู้ที่ต้องการตรวจสอบ
+    FightingStyles = {
+        {"Superhuman", "BuySuperhuman"},
+        {"DeathStep", "BuyDeathStep"},
+        {"SharkmanKarate", "BuySharkmanKarate"},
+        {"ElectricClaw", "BuyElectricClaw"},
+        {"DragonTalon", "BuyDragonTalon"},
+        {"Godhuman", "BuyGodhuman"},
+        {"SanguineArt", "BuySanguineArt"}
+    },
+
+    -- การตั้งค่าปุ่ม UI ที่จะใช้
+    UIButtons = {
+        {"CDK", "Cursed Dual Katana"},
+        {"Anchor", "Shark Anchor"},
+        {"SGA", "BuySanguineArt"},
+        {"GOD", "BuyGodhuman"},
+        {"Skarate", "BuySharkmanKarate"},
+        {"W3", "TravelZou"},
+        {"Race", "BlackbeardReward", "Reroll", "2"},
+        {"Cyborg", "CyborgTrainer", "Buy"},
+        {"Ghoul", "Ectoplasm", "Change", 4},
+        {"Friends", "Show Friends"}
+    }
+}
